1904441c91567aad69988d7996640eb12184f2f3
Maxon <notmax2000@mail.ru>
2022-10-25 22:15:07 +0700

    'v1'

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import re_path as path
+
+from . import views
+
+
+
+urlpatterns = [
+    path(r'^$', views.IndexView.as_view(), name='index'),
+    path(r'^registration/$', views.register, name='register'),
+    # path(r'^logged_out/$', views.logged_out, name='logged_out'),
+    # path(r'^login/$', views.login, name='login'),
+    path(r'^<int:pk>/$', views.DetailView.as_view(), name='detail'),
+    path(r'^<int:pk>/results/$', views.ResultsView.as_view(), name='results'),
+    path(r'^<int:question_id>/vote/$', views.vote, name='vote'),
+]
---
+++ polls/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
+
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'user_form': user_form})
+
+def logged_out(request):
+    return render(request, 'registration/logged_out.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'вы не сделали выбор'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
